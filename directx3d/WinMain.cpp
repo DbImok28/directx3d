672f0ddd0c39a565d10ac8cee83eb926d6870157
RubyCircle <2015gfif@gmail.com>
2020-07-27 17:47:23 +0300

    WndProc

--- directx3d/WinMain.cpp
+++ directx3d/WinMain.cpp
@@ -1,6 +1,20 @@
 #include <Windows.h>
 #include <exception>
 
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
+	default:
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(
 	HINSTANCE	hInstance,
 	HINSTANCE	hPrevInstance,
@@ -12,7 +26,7 @@ int CALLBACK WinMain(
 	WNDCLASSEX wc = {0};
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -49,6 +63,17 @@ int CALLBACK WinMain(
 	// show window
 	ShowWindow(hWnd, SW_SHOW);
 
-	while (true);
-	return 0;
+	// message
+
+	MSG msg;
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) != FALSE)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	if (gResult == -1)
+		return -1;
+	else
+		return msg.wParam;
 }
