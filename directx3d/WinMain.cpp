8acf96c4835f5449cc2094c4256ce86fae59b529
павел якушик <pavel.jakushik@yandex.by>
2020-08-07 22:10:31 +0300

    Window Messages

--- directx3d/WinMain.cpp
+++ directx3d/WinMain.cpp
@@ -1,40 +1,71 @@
 #include <Windows.h>
 #include <exception>
+#include <sstream>
+#include "WindowsMessageMap.h"
 
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+	static WindowsMessageMap mm;
+	OutputDebugStringA(mm(msg, lParam, wParam).c_str());
+
 	switch (msg)
 	{
 	case WM_CLOSE:
-		PostQuitMessage(0);
+		PostQuitMessage(EXIT_SUCCESS);
 		break;
-	default:
-		break;
+	case WM_KEYDOWN:
+	{
+
+	}
+	break;
+	case WM_KEYUP:
+	{
+
+	}
+	break;
+	case WM_CHAR:
+	{
+
+	}
+	break;
+	case WM_LBUTTONDOWN:
+	{
+		const POINTS pt = MAKEPOINTS(lParam);
+		std::ostringstream oss;
+		oss << "(" << pt.x << ", " << pt.y << ")\n";
+		SetWindowTextA(hWnd, oss.str().c_str());
+	}
+	break;
+	case WM_LBUTTONUP:
+	{
+
+	}
 	}
 	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
 
 int CALLBACK WinMain(
-	HINSTANCE	hInstance,
-	HINSTANCE	hPrevInstance,
-	LPSTR		lpCmdLine,
-	int			nCmdShow)
+								// CALLBACK #define для stdcall(соглашение для вызовов, вызываемый объект будет сам за собой очищать стек)
+	HINSTANCE	hInstance,		// указатель на начало исполняемого модуля, мз сигнатуры
+	HINSTANCE	hPrevInstance,	// для 16 битных систем
+	LPSTR		lpCmdLine,		// команды
+	int			nCmdShow)		// параметр отвечающий за то как будет показываться окно (свёрнуто, развёрнуто, на весь экран и т.д.).
 {
 	const auto pClassName = L"directx3d_window";
 	// register window class
 	WNDCLASSEX wc = {0};
 	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
+	wc.style = CS_OWNDC;
 	wc.hInstance = hInstance;
+	wc.lpszClassName = pClassName;
+	wc.lpfnWndProc = WndProc; 
 	wc.hIcon = nullptr;
 	wc.hCursor = nullptr;
 	wc.hbrBackground = nullptr;
 	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
 	wc.hIconSm = nullptr;
 
 	if (!RegisterClassEx(&wc))
@@ -55,7 +86,7 @@ int CALLBACK WinMain(
 		hInstance,
 		nullptr
 	);
-	if (hWnd == nullptr)
+	if (hWnd == INVALID_HANDLE_VALUE)
 	{
 		throw std::exception("Error to create window");
 	}
@@ -75,5 +106,5 @@ int CALLBACK WinMain(
 	if (gResult == -1)
 		return -1;
 	else
-		return msg.wParam;
+		return static_cast<int>(msg.wParam);
 }
